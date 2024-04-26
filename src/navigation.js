012c815e7f0344400ca09277a24ba94fb7d96519
Joerg Gamerdinger <joerg.gamerdinger@uni-tuebingen.de>
2024-04-26 14:04:25 +0200

    Header Weather.

--- src/navigation.js
+++ src/navigation.js
@@ -11,7 +11,7 @@ export const headerData = {
       href: getPermalink('/#scenarios')
     },
     {
-      text: 'Environmental Conditions',
+      text: 'Weather',
       href: getPermalink('/#environment'),
     },
     {
